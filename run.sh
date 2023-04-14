4ef74bbe1639fc0462a5f15fdcb3d6f681d1f317
Roy Conn <rmichael1001@gmail.com>
2023-04-14 13:16:17 -0400

    comments in run script

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
+# ------------------------------------------------
+#   Author(s): RMCJ < rmichael1001@gmail.com>
+#   Project: OpenDiscordBot
+#   Version: 1.0
+#
+#   Description: Clear terminal screen and run the bot
+#
+#   Instructions: ./run.sh
+#
+# ------------------------------------------------
+
 clear
 cd src
 ts-node ./index.ts
